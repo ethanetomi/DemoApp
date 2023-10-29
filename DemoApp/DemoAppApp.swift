dc1d428f79881f5bb030f073ad0850cf7a238401
Ethan Etomi <ethanetomi@icloud.com>
2023-10-29 00:04:17 -0400

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Ethan Etomi on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
